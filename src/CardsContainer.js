b3cea2a8724e6c1b5db97b88ef24ecbeabf6c624
Andrei Vaniukov <aovnkv@yahoo.com>
2019-04-02 17:41:08 +0400

    refactor: deleted state in CardsContainer

--- src/CardsContainer.js
+++ src/CardsContainer.js
@@ -2,46 +2,42 @@ import React, { Component } from 'react';
 import './CardsContainer.css';
 import CardItem from './CardItem';
 
-export default class CardsContainer extends Component {
-  state = {
-    items: [
-      {
-        id: 1,
-        topTitle: 'Сказочное заморское явство',
-        mTitle: 'Нямушка',
-        taste: 'с фуа-гра',
-        options: ['10 порций', 'мышь в подарок'],
-        weight: '0,5',
-        description: 'Печень утки разварная с артишоками.',
-        available: true
-      },
-      {
-        id: 2,
-        topTitle: 'Сказочное заморское явство',
-        mTitle: 'Нямушка',
-        taste: 'с рыбой',
-        options: ['40 порций', '2 мыши в подарок'],
-        weight: '2',
-        description: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
-        available: true
-      },
-      {
-        id: 3,
-        topTitle: 'Сказочное заморское явство',
-        mTitle: 'Нямушка',
-        taste: 'с курой',
-        options: ['100 порций', '5 мышей в подарок', 'заказчик доволен'],
-        weight: '5',
-        description: 'Филе из цыплят с трюфелями в бульоне.',
-        available: false
-      }
-    ]
-  };
+const cardItems = [
+  {
+    id: 1,
+    topTitle: 'Сказочное заморское явство',
+    mTitle: 'Нямушка',
+    taste: 'с фуа-гра',
+    options: ['10 порций', 'мышь в подарок'],
+    weight: '0,5',
+    description: 'Печень утки разварная с артишоками.',
+    available: true
+  },
+  {
+    id: 2,
+    topTitle: 'Сказочное заморское явство',
+    mTitle: 'Нямушка',
+    taste: 'с рыбой',
+    options: ['40 порций', '2 мыши в подарок'],
+    weight: '2',
+    description: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+    available: true
+  },
+  {
+    id: 3,
+    topTitle: 'Сказочное заморское явство',
+    mTitle: 'Нямушка',
+    taste: 'с курой',
+    options: ['100 порций', '5 мышей в подарок', 'заказчик доволен'],
+    weight: '5',
+    description: 'Филе из цыплят с трюфелями в бульоне.',
+    available: false
+  }
+];
 
+export default class CardsContainer extends Component {
   render() {
-    const items = this.state.items.map(item => (
-      <CardItem key={item.id} {...item} />
-    ));
+    const items = cardItems.map(item => <CardItem key={item.id} {...item} />);
     return <div className="CardsContainer">{items}</div>;
   }
 }
