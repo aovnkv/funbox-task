53f96f78618d3de5b2baf5d1b17e1dceaa7516b9
Andrei Vaniukov <aovnkv@yahoo.com>
2019-09-02 22:10:09 +0300

    Updates

---
+++ precache-manifest.c0e1ae1d401e3763618f8ce5e26f8e68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46318483f37cc9005a0554ffd5474aab",
+    "url": "/funbox-task/index.html"
+  },
+  {
+    "revision": "5346aa508e4034a50ba3",
+    "url": "/funbox-task/static/css/2.0971e70a.chunk.css"
+  },
+  {
+    "revision": "b35121e9d09945177ea4",
+    "url": "/funbox-task/static/css/main.d0272d52.chunk.css"
+  },
+  {
+    "revision": "5346aa508e4034a50ba3",
+    "url": "/funbox-task/static/js/2.62283e0f.chunk.js"
+  },
+  {
+    "revision": "b35121e9d09945177ea4",
+    "url": "/funbox-task/static/js/main.da41a276.chunk.js"
+  },
+  {
+    "revision": "2161f5ad055022355e87",
+    "url": "/funbox-task/static/js/runtime~main.1088f635.js"
+  },
+  {
+    "revision": "1d7cb3538ba2ff056172f66fcf8e51d5",
+    "url": "/funbox-task/static/media/Exo20-Thin.1d7cb353.eot"
+  },
+  {
+    "revision": "44fbebfce5c95819cb765809cfe24b71",
+    "url": "/funbox-task/static/media/Exo20-Thin.44fbebfc.woff"
+  },
+  {
+    "revision": "b8fd437dca4b38408c905606dd534024",
+    "url": "/funbox-task/static/media/Exo20-Thin.b8fd437d.ttf"
+  },
+  {
+    "revision": "66b5cf2055d3299c96c3d01c2006533a",
+    "url": "/funbox-task/static/media/cat_photo.66b5cf20.png"
+  }
+]);
